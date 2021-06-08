2280317fcc9092f9bc48378759a84e8205bae262
Philipp Gühring <pg@futureware.at>
2021-06-09 01:30:25 +0200

    Automatic environment variable setting

--- scripts/deploy2caravel.sh
+++ scripts/deploy2caravel.sh
@@ -4,7 +4,8 @@
 if [ -z "$CARAVEL" ]
 then
 	echo "Environment variables not found, please run '. env.sh' to define them."
-	exit
+	source env.sh || source ../env.sh
+	#exit
 fi
 
 mkdir $CARAVEL/cells{,/lib,/lef,/lef/orig,/gds,/mag}
@@ -13,10 +14,12 @@ rm -f $CARAVEL/cells/lib/*.lib $CARAVEL/cells/lef/orig/*.lef $CARAVEL/cells/lef/
 
 cd $STDCELLLIB/Catalog
 
+#perl $CARAVEL/scripts/drcexpander.pl $PDK_ROOT/sky130A/libs.tech/magic/sky130A.tech >$CARAVEL/sky130Aexpanded.tech
+#perl ../Tools/perl/drcfixall.pl $CARAVEL/sky130Aexpanded.tech
+
 cp *.lib $CARAVEL/cells/lib/
 cp outputlib/*.lef $CARAVEL/cells/lef/orig/
 cp outputlib/*.gds $CARAVEL/cells/gds/
-cp *.mag $CARAVEL/cells/mag/
 rm $CARAVEL/cells/mag/demoboard.mag
 
 cd $CARAVEL/cells/lef
@@ -29,6 +32,10 @@ mv new.lib libresilicon.lib
 perl $CARAVEL/scripts/generator.pl >$CARAVEL/verilog/rtl/user_proj_example.v
 perl $CARAVEL/scripts/cells.pl >$CARAVEL/verilog/rtl/user_proj_cells.v
 perl $CARAVEL/scripts/placement.pl >$CARAVEL/openlane/user_proj_example/macro_placement.cfg
+#for a in $CARAVEL/cells/mag/
+#do
+#  perl $STDCELLLIB/Tools/perl/drcfix.pl $a
+#done
 
 cd $CARAVEL
 #bash my.sh
