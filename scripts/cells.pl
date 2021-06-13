9a0c757be150d6d0522419e8ad09049561b6fbdd
Philipp Gühring <pg@futureware.at>
2021-06-13 20:17:39 +0200

    Added Power-Pin handling

--- scripts/cells.pl
+++ scripts/cells.pl
@@ -53,8 +53,10 @@ foreach my $mag(<$STDCELLLIB/Catalog/*.mag>)
 
   }
   close CELL;
+  print "     \`ifdef USE_POWER_PINS\n";
   print "	inout vdd, // cell power supply\n";
   print "	inout gnd  // cell ground supply\n";
+  print "     \'endif\n";
   print ");\n";
   print "endmodule\n\n";
 }
