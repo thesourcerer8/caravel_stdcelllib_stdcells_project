a5cbddf3347ac83a1561bee5be3718314805bfff
Philipp Gühring <pg@futureware.at>
2021-06-13 20:40:00 +0200

    Removed unused cells

--- scripts/cells.pl
+++ scripts/cells.pl
@@ -29,6 +29,8 @@ foreach my $mag(<$STDCELLLIB/Catalog/*.mag>)
   my $name=""; $name=$1 if($mag=~m/([\w\-\.]+)\.mag$/);
   next unless(-f $cell);
   next unless(-f $lib);
+  next unless(-f $ENV{'CARAVEL'}."/cells/mag/$name.mag");
+
   open CELL,"<$cell";
   print "module $name(\n";
 
