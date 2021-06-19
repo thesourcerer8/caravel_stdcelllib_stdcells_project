bb7dacf28ea7411f86b41d072428cb5d85516fd8
Philipp Gühring <pg@futureware.at>
2021-06-19 04:33:31 +0200

    New cells

--- cells/lef/fixup.pl
+++ cells/lef/fixup.pl
@@ -3,12 +3,29 @@ use strict;
 
 my %layersToDo=("li1"=>1,"mcon"=>1,"locali"=>1,"metal1"=>1);
 
+sub readfile($)
+{
+  if(open MYRIN,"<$_[0]")
+  {
+    my $old=$/;
+    undef $/;
+    my $content=<MYRIN>;
+    close MYRIN;
+    $/=$old;
+    return $content;
+  }
+  return undef;
+}
+
+
 foreach my $origlef (<orig/*.lef>)
 {
   my $lef=$origlef; $lef=~s/^orig\///;	
   my $mag="../mag/$lef"; $mag=~s/\.lef$/\.mag/;
   print STDERR "origlef: $origlef\n lef: $lef\n mag:$mag\n";
   my $obs="";
+
+
 
   if(open(MAGIN,"<$mag"))
   {
@@ -23,7 +40,7 @@ foreach my $origlef (<orig/*.lef>)
       }
       if(m/rect (-?\d+) (-?\d+) (-?\d+) (-?\d+)/ && $active)
       {
-        $obs.="RECT ( ".($1/100.0)." ".($2/100.0)." ) ( ".($3/100.0)." ".($4/100.0)." ) ;\n";
+        $obs.="      RECT ( ".($1/100.0)." ".($2/100.0)." ) ( ".($3/100.0)." ".($4/100.0)." ) ;\n";
       }
     }
     close MAGIN;
@@ -35,18 +52,29 @@ foreach my $origlef (<orig/*.lef>)
 
   #print "$mag\n$obs\n";
   #next;
+  
+  my $content=readfile("orig/$lef");
+  $content=~s/\sOBS\s.*?\sEND\s//sg;
+  #open OUT,">$lef.temp";
+  #print OUT $content;
+  #close OUT;
 
   my $cell=$lef; $cell=~s/\.lef//;
   print "$lef\n";
-  open LEFIN,"<orig/$lef";
+  #  open LEFIN,"<orig/$lef";
   open LEFOUT,">$lef";
   our $pin="";
   my $filled=0;
-  while(<LEFIN>)
+  our $macro="NOTTOBEFOUND";
+
+  #print "$obs\n";
+  foreach(split "\n",$content) # while(<LEFIN>)
   {
+    $_.="\n";	  
+    $macro=$1 if(m/MACRO\s+(\w+)/);
     $pin=$1 if(m/PIN (\w+)/);	  
-    s/SYMMETRY X Y R90/SITE unit/;
-    s/SITE CORE/SYMMETRY X Y R90/;
+    #s/SYMMETRY X Y R90/SITE unit/;
+    #s/SITE CORE/SYMMETRY X Y R90/;
     #s/SITE unit.*//;
     s/metal2/met1/;
     s/VDD/vdd/;
@@ -54,34 +82,42 @@ foreach my $origlef (<orig/*.lef>)
     s/USE SIGNAL/USE POWER/ if($pin eq "VDD");
     s/USE SIGNAL/USE GROUND/ if($pin eq "GND");
 
-    if(m/^END (\w+)/ && !$filled)
+    if(m/^\s*END\s+$macro/ && !$filled) # Inject the obstructions here
     {
-      print LEFOUT "  OBS\n$obs\n  end\n";
+      print LEFOUT "\n  OBS\n$obs\n  end\n";
+      $filled=1;
     }
-
+    #print $_;
     print LEFOUT $_;	   
+    #print "$macro\n";
     #print $_;
     if(m/FOREIGN/)
     {
       if(open(MAG,"<$mag"))
       {
+        my $layer="";
         while(<MAG>)
         {
+          if(m/<< (\w+) >>/)
+	  {
+            $layer=$1;
+	  }
           if(m/rect 0 0 (\d+) (\d+)/)
           {
+            next if($layer eq "checkpaint");
             my $h=$1;
             my $w=$2;
             my $factor=0.01;
-            print "$lef -> ".($1*$factor)." ".($2*$factor)."\n";
-            print LEFOUT " SIZE ".($1*$factor)." BY ".($2*$factor)." ;\n";
+	    #print "$lef -> ".($1*$factor)." ".($2*$factor)."\n";
+	    #print LEFOUT " SIZE ".($1*$factor)." BY ".($2*$factor)." ;\n";
           }
           if(m/string FIXED_BBOX 0 0 (\d+) (\d+)/)
           {
             my $h=$1;
             my $w=$2;
             my $factor=0.01;
-            print "$lef -> ".($1*$factor)." ".($2*$factor)."\n";
-            print LEFOUT " SIZE ".($1*$factor)." BY ".($2*$factor)." ;\n";
+	    #print "$lef -> ".($1*$factor)." ".($2*$factor)."\n";
+	    #print LEFOUT " SIZE ".($1*$factor)." BY ".($2*$factor)." ;\n";
           }
 
 	}
@@ -90,7 +126,9 @@ foreach my $origlef (<orig/*.lef>)
     }
   }
   close LEFOUT;
-  close LEFIN;
+  #close LEFIN;
+
+  system "cp $lef $lef.beforemagic";
 
   open MAGIC,"|magic -dnull -noconsole -T sky130A";
   print MAGIC "lef read $lef\n";
