51a9d2872827d52aec0c7430e06e53f9d5cb6d77
Philipp Gühring <pg@futureware.at>
2021-06-25 17:02:23 +0200

    Script to automate the git add process for new cells

---
+++ scripts/gitpush.sh
@@ -0,0 +1,2 @@
+git add cells/ def/user_proj_example.def gds/user_proj_example.gds.gz gds/user_project_wrapper.gds.gz lef/user_proj_example.lef mag/user_project_wrapper.mag openlane/user_proj_example/config.tcl openlane/user_proj_example/macro_placement.cfg signoff/user_proj_example/final_summary_report.csv verilog/rtl/user_proj_cells.v verilog/rtl/user_proj_example.v
+
