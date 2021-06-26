6bd82d7e822ac1923eaa0ff25eddcbf16412e3b7
Philipp Gühring <pg@futureware.at>
2021-06-26 13:36:10 +0200

    Improved handling of temporarily nonexisting files

--- scripts/gitpush.sh
+++ scripts/gitpush.sh
@@ -1,2 +1,4 @@
-git add cells/ def/user_proj_example.def gds/user_proj_example.gds.gz gds/user_project_wrapper.gds.gz lef/user_proj_example.lef mag/user_project_wrapper.mag openlane/user_proj_example/config.tcl openlane/user_proj_example/macro_placement.cfg signoff/user_proj_example/final_summary_report.csv verilog/rtl/user_proj_cells.v verilog/rtl/user_proj_example.v
-
+git add cells/ def/user_proj_example.def lef/user_proj_example.lef openlane/user_proj_example/config.tcl openlane/user_proj_example/macro_placement.cfg signoff/user_proj_example/final_summary_report.csv verilog/rtl/user_proj_cells.v verilog/rtl/user_proj_example.v
+git add gds/user_proj_example.gds.gz 
+git add gds/user_project_wrapper.gds.gz 
+git add mag/user_project_wrapper.mag 
