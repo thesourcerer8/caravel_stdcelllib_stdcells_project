4c2dc20574619579be40257c757557c8d1228449
Philipp Gühring <pg@futureware.at>
2021-11-13 17:07:43 +0100

    Migrating to MPW3

--- env.sh
+++ env.sh
@@ -1,10 +1,10 @@
 export STD_CELL_LIBRARY=sky130_fd_sc_ls
 export STDCELLLIB=/home/philipp/libresilicon/StdCellLib
 export OPENLANE_ROOT=$(readlink -f $(pwd)/../openlane )
-export OPENLANE_TAG=v0.15
+export OPENLANE_TAG=mpw-3a
 export CARAVEL=$(pwd)
 export CARAVEL_ROOT=$(pwd)/caravel
 export PDK_ROOT=$(readlink -f $(pwd)/../pdk )
-#export PDK_ROOT=$(pwd)/../pdk
+export PDK_ROOT=$(pwd)/../pdk
 export PATH=$PATH:$(readlink -f $(pwd)../openlane_summary/ )
 
